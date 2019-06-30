ee37273b1fca0ac71059e41f16f137454a89f128
CobbyBaf <51709602+CobbyBaf@users.noreply.github.com>
2019-06-30 09:48:57 +0100

    30-june-2019

---
+++ untitled0.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 14:40:49 2019
+
+@author: HI
+"""
+
+a=9.6
+b=2
+
+a//b
+
+round(0.5)-round(-0.5)
+import math 
+round(math.pi, 200)
+ divmod(10,5.5)
+d={'pooja':24, 'akhil':30}
+d['akhil']
+n1 = {5,6,7}
+n1.intersection_update({7,10,3,5})
+s2={7,8,9}
+s2.issubset(s2)
